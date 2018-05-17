088b76a21337ed45664c3ea3e25c6bfc39c042da
Caleb McCarthy <caleb.a.mccarthy@gmail.com>
2018-05-17 08:57:13 -0500

    initial commit

---
+++ pullall.ps1
@@ -0,0 +1,18 @@
+cd C:\development\vh-web-caregiverportal
+
+git pull
+
+cd C:\development\vivify-platform
+
+git pull
+
+
+cd C:\development\vivify-platform\Database; ./MigrateDatabase.bat
+
+cd C:\development\vivify-kitting-platform\Vivify.Kit.Database;
+
+git pull
+
+./MigrateDatabase kittingportal
+
+
